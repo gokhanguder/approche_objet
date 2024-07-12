3355bcdc6281acadcad1e51a8606fb4988b71ee4
Gokhan <gguder@gmail.com>
2024-07-12 12:36:50 +0200

    TP2

---
+++ src/main/java/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,32 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+
+    public static void main(String[] args){
+
+        byte b = 1;
+        short s = 20;
+        int i = 300;
+        long l = 2000000000;
+        float f = 2.0005F;
+        double d = 2.000055555;
+        char c= 'c';
+        boolean bo = true;
+
+        String s1 = "helloworld";
+
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(bo);
+        System.out.println(s1);
+
+        String randomString = "Voici le résultat d’un calcul :\n1+5=6";
+        System.out.println(randomString);
+
+    }
+}
