ad6f76c478fc0d3e9a968757c82e98673c97046b
gudertesteur <gudertesteur@gmail.com>
2024-07-16 15:58:11 +0200

    TP15 - Equals

---
+++ src/main/java/fr/diginamic/chaines/TestEquals.java
@@ -0,0 +1,39 @@
+package fr.diginamic.chaines;
+
+public class TestEquals {
+    public static void main(String[] args) {
+
+        Ville ville1 = new Ville(30000, "Nimes");
+        Ville ville2 = new Ville(30000, "Nimes");
+
+        if (ville1 == ville2) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+
+        if (ville1.equals(ville2)) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+
+        // Modifier un attribut et retester equals
+        ville2.setNbrHabitants(40000);
+
+        if (ville1.equals(ville2)) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+
+        // Réinitialiser ville2 et retester avec ==
+        ville2 = ville1;
+        if (ville1 == ville2) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+
+        }
+    }
+}
---
+++ src/main/java/fr/diginamic/chaines/Ville.java
@@ -0,0 +1,40 @@
+package fr.diginamic.chaines;
+
+public class Ville {
+
+    public String nom;
+    public int nbrHabitants;
+
+    public Ville(int nbrHabitants, String nom) {
+        this.nbrHabitants = nbrHabitants;
+        this.nom = nom;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getNbrHabitants() {
+        return nbrHabitants;
+    }
+
+    public void setNbrHabitants(int nbrHabitants) {
+        this.nbrHabitants = nbrHabitants;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Ville ville = (Ville) obj;
+        return nbrHabitants == ville.nbrHabitants && nom.equals(ville.nom);
+    }
+}
